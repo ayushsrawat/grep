b09f4a49e554e9541f230ec0359464bf306c2ca7
ayushrawat <ayushrawat3338@gmail.com>
2025-02-23 09:50:08 +0530

    builder, removed log4j2

--- src/main/java/com/github/drsqrt/Grep.java
+++ src/main/java/com/github/drsqrt/Grep.java
@@ -6,8 +6,6 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -21,6 +19,11 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.EnumSet;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Grep is a search tool <br>
@@ -29,10 +32,14 @@ import java.util.EnumSet;
  */
 public class Grep {
 
-  private static final Logger logger = LogManager.getLogger(Grep.class);
+  private static final Logger logger = Logger.getLogger(Grep.class.getName());
   private static final char TILDE = '~';
   private static CommandLineArguments commandLineArguments;
   private static boolean VERBOSE = false;
+
+  static {
+    configLogger();
+  }
 
   public static void main(String[] args) {
     try {
@@ -49,8 +56,25 @@ public class Grep {
       grep.validateCommandLineArguments();
       grep.execute();
     } catch (ParseException parseException) {
-      logger.error(parseException);
+      logger.severe(parseException.getMessage());
     }
+  }
+
+  private static void configLogger() {
+    Logger rootLogger = Logger.getLogger("");
+    Handler[] handlers = rootLogger.getHandlers();
+    for (Handler h : handlers) {
+      rootLogger.removeHandler(h);
+    }
+
+    ConsoleHandler consoleHandler = new ConsoleHandler();
+    consoleHandler.setFormatter(new SimpleFormatter() {
+      @Override
+      public String format(LogRecord record) {
+        return String.format("%s%n", record.getMessage());
+      }
+    });
+    rootLogger.addHandler(consoleHandler);
   }
 
   private static Options createOptions() {
@@ -65,7 +89,11 @@ public class Grep {
     String searchKeyword = cli.getOptionValue('s');
     String filePath = cli.getOptionValue('f');
     boolean verbose = cli.hasOption('v');
-    return new CommandLineArguments(searchKeyword, filePath, verbose);
+    return CommandLineArguments.builder()
+      .searchKeyword(searchKeyword)
+      .filePath(filePath)
+      .verbose(verbose)
+      .build();
   }
 
   private void validateCommandLineArguments() {
@@ -88,20 +116,20 @@ public class Grep {
   }
 
   private void execute() {
-    if (VERBOSE) logger.info("Search Keyword : {}", commandLineArguments.getSearchKeyword());
-    if (VERBOSE) logger.info("File Path : {} \n\n", commandLineArguments.getFilePath());
+    if (VERBOSE) logger.info("Search Keyword : " + commandLineArguments.getSearchKeyword());
+    if (VERBOSE) logger.info("File Path :  " + commandLineArguments.getFilePath() + "\n\n");
     Path filePath = Paths.get(commandLineArguments.getFilePath());
     try {
       Files.walkFileTree(filePath, EnumSet.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE, new SimpleFileVisitor<>() {
         @Override
         public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-          if (VERBOSE) logger.info("Visiting File : {}", path.toAbsolutePath());
+          if (VERBOSE) logger.info("Visiting File : " + path.toAbsolutePath());
           searchInFile(path);
           return super.visitFile(path, attrs);
         }
       });
     } catch (IOException e) {
-      logger.error(e);
+      logger.severe(e.getMessage());
     }
   }
 
@@ -112,12 +140,12 @@ public class Grep {
       int lineNumber = 1;
       while ((line = reader.readLine()) != null) {
         if (line.contains(commandLineArguments.getSearchKeyword())) {
-          logger.info("{}:{}: {}", file.getAbsolutePath(), lineNumber, line);
+          logger.info(file.getAbsolutePath() + ":" + lineNumber + " : " + line);
         }
         lineNumber++;
       }
     } catch (IOException e) {
-      logger.error("Failed to read file: {}", e.getMessage());
+      logger.severe("Failed to read file: " + e.getMessage());
     }
   }
 
@@ -127,10 +155,14 @@ public class Grep {
     private String filePath;
     private final Boolean verbose;
 
-    protected CommandLineArguments(String searchKeyword, String filePath, boolean verbose) {
-      this.searchKeyword = searchKeyword;
-      this.filePath = filePath;
-      this.verbose = verbose;
+    protected CommandLineArguments(Builder builder) {
+      this.searchKeyword = builder.searchKeyword;
+      this.filePath = builder.filePath;
+      this.verbose = builder.verbose;
+    }
+
+    public static Builder builder() {
+      return new Builder();
     }
 
     public String getSearchKeyword() {
@@ -147,6 +179,35 @@ public class Grep {
 
     public void setFilePath(String filePath) {
       this.filePath = filePath;
+    }
+
+    public static final class Builder {
+
+      private String searchKeyword;
+      private String filePath = ".";
+      private Boolean verbose = false;
+
+      public CommandLineArguments build() {
+        if (searchKeyword == null || searchKeyword.isEmpty()) {
+          throw new IllegalArgumentException("Search Keyword is required.");
+        }
+        return new CommandLineArguments(this);
+      }
+
+      public Builder searchKeyword(String searchKeyword) {
+        this.searchKeyword = searchKeyword;
+        return this;
+      }
+
+      public Builder filePath(String filePath) {
+        this.filePath = filePath;
+        return this;
+      }
+
+      public Builder verbose(Boolean verbose) {
+        this.verbose = verbose;
+        return this;
+      }
     }
   }
 
