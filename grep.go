1dd9c34c6f1ec668944b6e64c379c4ca7b4645aa
Ayush Rawat <ayushrawat3338@gmail.com>
2025-05-08 23:10:24 +0530

    Parse CLI options

--- grep.go
+++ grep.go
@@ -1,34 +1,58 @@
 package main
 
 import (
-	"io/fs"
+	"bufio"
+	"errors"
+	"io"
+	ifs "io/fs"
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
+
+	flag "github.com/spf13/pflag"
 )
 
-func usage() {
+func usage(fs *flag.FlagSet) {
 	log.Print("usage: grep [-rnvH] <search-keyword> <where-to-search>")
+	fs.PrintDefaults()
 }
 
 func main() {
-	args := os.Args[1:]
 	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
-	if len(args) < 1 {
-		usage()
-		log.Fatal("ERROR: No command is provided")
-	}
 
-	// todo: support [-rnvH] & [-r -n -v -H] & [--recusive --new-line --version] commands
+	// todo: support [-rnvH] & [-r -n -v -H] & [--recursive --new-line --version] commands
+	fs := flag.NewFlagSet("grep", flag.ContinueOnError)
+	fs.SetOutput(os.Stdout)
+
+	help := fs.BoolP("help", "H", false, "Print a brief help message.")
+	recursive := fs.BoolP("recursive", "r", false, "Recursively search subdirectories listed.")
+	lineNumber := fs.BoolP("line-number", "n", false, "Each output line is preceded by its relative line number in the file, starting at line 1.  The line number counter is reset for each file processed.")
+
+	if err := fs.Parse(os.Args[1:]); err != nil {
+		usage(fs)
+		os.Exit(1)
+	}
+	args := fs.Args()
+	if len(args) < 1 || *help {
+		usage(fs)
+		os.Exit(0)
+	}
 
 	keyword := args[0]
 	where := "."
 	if len(args) > 1 {
 		where = args[1]
 	}
-	log.Print("Keyword: ", keyword)
-	log.Print("Where: ", where)
-	filepath.WalkDir(where, func(path string, d fs.DirEntry, err error) error {
+	// log.Printf("Keyword: %s", keyword)
+	// log.Printf("Where: %s", where)
+	if *recursive {
+		log.Print("Provided recursive flag")
+	}
+	if *lineNumber {
+		log.Print("Provided line number flag")
+	}
+	filepath.WalkDir(where, func(path string, d ifs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
@@ -36,10 +60,35 @@ func main() {
 		if d.Type().IsRegular() {
 			f, err := os.Open(path)
 			if err != nil {
-				log.Print("Error reading file: ", f.Name())
+				log.Printf("Error reading file: %s", f.Name())
 				return err
 			}
-			// todo: read this file and regex the searched keyword
+			defer f.Close()
+			reader := bufio.NewReader(f)
+			searchRegex := `([\w]*)` + regexp.QuoteMeta(keyword) + `([\w]*)`
+			reg, err := regexp.Compile(searchRegex)
+			if err != nil {
+				log.Fatalf("Invalid regex: %v", err)
+			}
+			lineNumber := 0
+			for {
+				line, err := reader.ReadString('\n')
+				if err != nil {
+					if errors.Is(err, io.EOF) {
+						break
+					}
+					if err.Error() == "EOF" {
+						break
+					}
+					log.Fatal("error reading file: ", f)
+				}
+				lineNumber++
+
+				// todo: https://github.com/fatih/color
+				if reg.MatchString(line) {
+					log.Printf("%d: %s", lineNumber, line)
+				}
+			}
 		}
 		return nil
 	})
