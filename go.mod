f6d7ac3f2a94cf2b288a8cf5076afedf2c25046e
Ayush Rawat <ayushrawat3338@gmail.com>
2025-05-10 22:20:50 +0530

    support recursive & showln flags

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/ayushsrawat/grep
 
 go 1.23.4
 
-require github.com/spf13/pflag v1.0.6
+require (
+	github.com/fatih/color v1.18.0
+	github.com/spf13/pflag v1.0.6
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- grep.go
+++ grep.go
@@ -9,9 +9,20 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
+	"strings"
 
+	"github.com/fatih/color"
 	flag "github.com/spf13/pflag"
 )
+
+type cmd struct {
+	recursive bool
+	showln    bool
+
+	keyword string
+	where   string
+}
 
 func usage(fs *flag.FlagSet) {
 	log.Print("usage: grep [-rnvH] <search-keyword> <where-to-search>")
@@ -25,9 +36,9 @@ func main() {
 	fs := flag.NewFlagSet("grep", flag.ContinueOnError)
 	fs.SetOutput(os.Stdout)
 
-	help := fs.BoolP("help", "H", false, "Print a brief help message.")
+	help := fs.BoolP("help", "h", false, "Print a brief help message and exit.")
 	recursive := fs.BoolP("recursive", "r", false, "Recursively search subdirectories listed.")
-	lineNumber := fs.BoolP("line-number", "n", false, "Each output line is preceded by its relative line number in the file, starting at line 1.  The line number counter is reset for each file processed.")
+	showln := fs.BoolP("line-number", "n", false, "Each output line is preceded by its relative line number in the file, starting at line 1.  The line number counter is reset for each file processed.")
 
 	if err := fs.Parse(os.Args[1:]); err != nil {
 		usage(fs)
@@ -44,52 +55,73 @@ func main() {
 	if len(args) > 1 {
 		where = args[1]
 	}
-	// log.Printf("Keyword: %s", keyword)
-	// log.Printf("Where: %s", where)
-	if *recursive {
-		log.Print("Provided recursive flag")
+
+	c := cmd{
+		recursive: *recursive,
+		showln:    *showln,
+		keyword:   keyword,
+		where:     where,
 	}
-	if *lineNumber {
-		log.Print("Provided line number flag")
-	}
+
+	// todo: read the .hidden files after normal files
 	filepath.WalkDir(where, func(path string, d ifs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
-
-		if d.Type().IsRegular() {
-			f, err := os.Open(path)
-			if err != nil {
-				log.Printf("Error reading file: %s", f.Name())
-				return err
+		if d.IsDir() {
+			if *recursive {
+				return nil
 			}
-			defer f.Close()
-			reader := bufio.NewReader(f)
-			searchRegex := `([\w]*)` + regexp.QuoteMeta(keyword) + `([\w]*)`
-			reg, err := regexp.Compile(searchRegex)
-			if err != nil {
-				log.Fatalf("Invalid regex: %v", err)
-			}
-			lineNumber := 0
-			for {
-				line, err := reader.ReadString('\n')
-				if err != nil {
-					if errors.Is(err, io.EOF) {
-						break
-					}
-					if err.Error() == "EOF" {
-						break
-					}
-					log.Fatal("error reading file: ", f)
-				}
-				lineNumber++
-
-				// todo: https://github.com/fatih/color
-				if reg.MatchString(line) {
-					log.Printf("%d: %s", lineNumber, line)
-				}
-			}
+			return ifs.SkipDir
 		}
-		return nil
+		return searchInFile(path, c)
 	})
+}
+
+func searchInFile(file string, cmd cmd) error {
+	green := color.New(color.FgGreen).SprintFunc()
+	magenta := color.New(color.FgHiMagenta).SprintFunc()
+	cyan := color.New(color.FgCyan).SprintFunc()
+
+	f, err := os.Open(file)
+	if err != nil {
+		log.Printf("Error reading file: %s", f.Name())
+		return err
+	}
+	defer f.Close()
+	reader := bufio.NewReader(f)
+	searchRegex := `([\w]*)` + regexp.QuoteMeta(cmd.keyword) + `([\w]*)`
+	reg, _ := regexp.Compile(searchRegex)
+
+	lineNumber := 0
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			if err.Error() == "EOF" {
+				break
+			}
+			log.Fatal("error reading file: ", f)
+		}
+		lineNumber++
+
+		line = strings.TrimRight(line, "\r\n")
+		// line = strings.TrimSpace(line)
+		if reg.MatchString(line) {
+			// todo: highlight the searched text from line
+			var sb strings.Builder
+			sb.WriteString(green(file))
+			if cmd.showln {
+				sb.WriteString(":")
+				sb.WriteString(magenta(strconv.Itoa(lineNumber)))
+
+			}
+			sb.WriteString(": ")
+			sb.WriteString(cyan(line))
+			log.Print(sb.String())
+		}
+	}
+	return nil
 }
